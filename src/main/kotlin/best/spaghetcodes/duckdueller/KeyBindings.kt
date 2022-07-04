806d3befa0aff4de4f029c9fc3251958fc135de8
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-04 19:01:07 +0200

    created KeyBindings class and en_US lang file

---
+++ src/main/kotlin/best/spaghetcodes/duckdueller/KeyBindings.kt
@@ -0,0 +1,14 @@
+package best.spaghetcodes.duckdueller
+
+import net.minecraft.client.settings.KeyBinding
+import net.minecraftforge.fml.client.registry.ClientRegistry
+import org.lwjgl.input.Keyboard
+
+object KeyBindings {
+    private val toggleBotKeyBinding: KeyBinding = KeyBinding("duck.toggleBot", Keyboard.KEY_SEMICOLON, "category.duck")
+
+    fun registerKeyBindings() {
+        println("Registering key bindings")
+        ClientRegistry.registerKeyBinding(toggleBotKeyBinding)
+    }
+}
