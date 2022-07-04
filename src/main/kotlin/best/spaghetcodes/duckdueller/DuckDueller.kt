4b0d96295ef11714d476f9bf13e8010c2ad970fb
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-04 19:01:49 +0200

    register keybindings, removed useless stuff and added the language adapter

--- src/main/kotlin/best/spaghetcodes/duckdueller/DuckDueller.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/DuckDueller.kt
@@ -1,23 +1,27 @@
 package best.spaghetcodes.duckdueller
 
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 
-public const val NAME = "Duck Dueller"
-public const val VERSION = "0.1.0"
-public const val MOD_ID = "duckdueller"
+const val NAME = "Duck Dueller"
+const val VERSION = "0.1.0"
+const val MOD_ID = "assets/lang/duckdueller"
 
 @Mod(
     modid = MOD_ID,
     name = NAME,
-    version = VERSION
+    version = VERSION,
+    modLanguageAdapter = "best.spaghetcodes.duckdueller.adapter.KotlinLanguageAdapter"
 )
-class DuckDueller {
+object DuckDueller {
 
-    @EventHandler
+    private val eventHandler: EventHandler = EventHandler()
+
+    @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
         println("Duck Dueller v$VERSION is loading...")
+
+        KeyBindings.registerKeyBindings()
     }
 
 }
