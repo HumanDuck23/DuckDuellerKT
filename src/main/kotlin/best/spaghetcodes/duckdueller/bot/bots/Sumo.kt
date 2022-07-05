fd4e037b43052ce7816d2285f21297e2dc314e5f
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-05 12:39:58 +0200

    created empty Sumo.kt class

---
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/bots/Sumo.kt
@@ -0,0 +1,11 @@
+package best.spaghetcodes.duckdueller.bot.bots
+
+import best.spaghetcodes.duckdueller.bot.BotBase
+
+class Sumo : BotBase("Opponent: ", "Accuracy", "/play duels_sumo_duel") {
+
+    override fun getName(): String {
+        return "Sumo"
+    }
+
+}
