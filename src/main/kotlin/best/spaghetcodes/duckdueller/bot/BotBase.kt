66f39e300039206d41905cf5330b52c1a8312e41
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-05 12:39:25 +0200

    created BotBase.kt to provide base for all bots and allow for easy customization

---
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/BotBase.kt
@@ -0,0 +1,133 @@
+package best.spaghetcodes.duckdueller.bot
+
+import best.spaghetcodes.duckdueller.DuckDueller
+import best.spaghetcodes.duckdueller.control.KeyBindings
+import best.spaghetcodes.duckdueller.utils.ChatUtils
+import best.spaghetcodes.duckdueller.utils.Config
+import best.spaghetcodes.duckdueller.utils.EntityUtils
+import best.spaghetcodes.duckdueller.utils.TimeUtils
+import net.minecraft.client.Minecraft
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.EnumChatFormatting
+import net.minecraftforge.client.event.ClientChatReceivedEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
+import java.util.*
+
+/**
+ * Base class for all bots
+ *
+ * Simply extend this class and override some methods
+ */
+open class BotBase protected constructor(val startMessage: String, val stopMessage: String, val queueCommand: String) {
+
+    protected val mc = Minecraft.getMinecraft();
+    protected var opponent: EntityPlayer? = null
+
+    protected var toggled = false
+
+    private var calledFoundOpponent = false
+    private var opponentTimer: Timer? = null
+
+    // These need to be overridden by subclasses to customize the bots behavior
+    open fun getName(): String {
+        return "Base"
+    }
+
+    /**
+     * Called when the bot attacks a player
+     */
+    protected open fun onAttack() {}
+
+    /**
+     * Called when the bot is attacked
+     */
+    protected open fun onAttacked() {}
+
+    /**
+     * Called when the game starts
+     */
+    protected open fun onGameStart() {}
+
+    /**
+     * Called when the game ends
+     */
+    protected open fun onGameEnd() {}
+
+    /**
+     * Called when the opponent entity is found
+     */
+    protected open fun onFoundOpponent() {}
+
+    // Public base methods for every bot
+
+    fun toggle() {
+        toggled = !toggled
+    }
+
+    fun isToggled() = toggled
+
+    // Private backend bot stuff
+
+    private fun _gameStart() {
+        val quickRefreshTimer: Timer? = TimeUtils.setInterval(this::bakery, 200, 50)
+        TimeUtils.setTimeout(fun () {
+            quickRefreshTimer?.cancel()
+            opponentTimer = TimeUtils.setInterval(this::bakery, 0, 5000)
+        }, 2000)
+
+        onGameStart()
+    }
+
+    private fun bakery() { // yes.
+        // yes this is a feature
+        val foundOpponent = getOpponentEntity()
+        if (foundOpponent && !calledFoundOpponent) {
+            calledFoundOpponent = true
+            onFoundOpponent()
+        }
+    }
+
+    private fun getOpponentEntity(): Boolean {
+        opponent = EntityUtils.getOpponentEntity()
+        return opponent != null
+    }
+
+    private fun _gameEnd() {
+        opponent = null
+        calledFoundOpponent = false
+        opponentTimer?.cancel()
+
+        onGameEnd()
+
+        TimeUtils.setTimeout(fun () {
+            DuckDueller.mc.thePlayer.sendChatMessage(Config.get("ggMessage") as String? ?: "GG")
+            TimeUtils.setTimeout(fun () {
+                best.spaghetcodes.duckdueller.bot.player.Queue.joinGame(queueCommand)
+            }, Config.get("rqDelay") as Int)
+        }, Config.get("ggDelay") as Int)
+    }
+
+    @SubscribeEvent
+    fun onChatMessage(ev: ClientChatReceivedEvent) {
+        if (isToggled()) {
+            val unformatted = ev.message.unformattedText
+
+            if (unformatted.contains(startMessage)) {
+                _gameStart()
+            } else if (unformatted.contains(stopMessage)) {
+                _gameEnd()
+            }
+        }
+    }
+
+    @SubscribeEvent
+    fun onTick(ev: ClientTickEvent) {
+        if (KeyBindings.toggleBotKeyBinding.isPressed) {
+            toggle()
+            ChatUtils.info("Duck Dueller has been toggled ${if (isToggled()) "${EnumChatFormatting.GREEN}on" else "${EnumChatFormatting.RED}off"}")
+            if (isToggled()) ChatUtils.info("Current selected bot: ${EnumChatFormatting.BOLD}${EnumChatFormatting.GREEN}${getName()}${EnumChatFormatting.RESET}")
+        }
+    }
+
+}
