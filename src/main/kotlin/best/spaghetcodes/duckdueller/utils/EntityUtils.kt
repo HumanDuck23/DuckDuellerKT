5782fd1e5498caa4422e412e474f7af732987f7c
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-05 13:45:20 +0200

    created EntityUtils.getRotations method

--- src/main/kotlin/best/spaghetcodes/duckdueller/utils/EntityUtils.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/utils/EntityUtils.kt
@@ -1,8 +1,10 @@
 package best.spaghetcodes.duckdueller.utils
 
 import best.spaghetcodes.duckdueller.DuckDueller
-import best.spaghetcodes.duckdueller.bot.BotBase
+import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityLivingBase
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.util.MathHelper
 
 object EntityUtils {
 
@@ -32,6 +34,41 @@ object EntityUtils {
             }
         } else {
             false
+        }
+    }
+
+    /**
+     * Get the rotations needed to look at an entity
+     *
+     * *Not originally my code, but I forgot where I found it.*
+     *
+     * @param target target entity
+     * @param raw If true, only returns difference in yaw and pitch instead of values needed
+     * @return float[] - {yaw, pitch}
+     */
+    fun getRotations(player: EntityPlayer?, target: Entity?, raw: Boolean): FloatArray? {
+        return if (target == null || player == null) {
+            null
+        } else {
+            val diffX = target.posX - player.posX
+            val diffY: Double = if (target is EntityLivingBase) {
+                target.posY + target.eyeHeight.toDouble() - (player.posY + player.getEyeHeight().toDouble())
+            } else {
+                (target.entityBoundingBox.minY + target.entityBoundingBox.maxY) / 2.0 - (player.posY + player.getEyeHeight().toDouble())
+            }
+            val diffZ = target.posZ - player.posZ
+            val dist = MathHelper.sqrt_double(diffX * diffX + diffZ * diffZ).toDouble()
+            val yaw = (Math.atan2(diffZ, diffX) * 180.0 / 3.141592653589793).toFloat() - 90.0f
+            val pitch = (-(Math.atan2(diffY, dist) * 180.0 / 3.141592653589793)).toFloat()
+            if (raw) {
+                floatArrayOf(
+                    MathHelper.wrapAngleTo180_float(yaw - player.rotationYaw),
+                    MathHelper.wrapAngleTo180_float(pitch - player.rotationPitch)
+                )
+            } else floatArrayOf(
+                player.rotationYaw + MathHelper.wrapAngleTo180_float(yaw - player.rotationYaw),
+                player.rotationPitch + MathHelper.wrapAngleTo180_float(pitch - player.rotationPitch)
+            )
         }
     }
 
