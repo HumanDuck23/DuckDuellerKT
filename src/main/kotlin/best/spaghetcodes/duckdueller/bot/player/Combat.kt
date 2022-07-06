1958c556577d91d162676a0168673affda0c5ac2
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-06 22:36:36 +0200

    created Combat randomStrafe method

--- src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/Combat.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/Combat.kt
@@ -5,6 +5,8 @@ import best.spaghetcodes.duckdueller.utils.TimeUtils
 
 object Combat {
 
+    private var randomStrafe = false
+
     fun wTap(duration: Int) {
         Movement.stopForward()
         TimeUtils.setTimeout(Movement::startForward, duration)
@@ -33,6 +35,30 @@ object Combat {
     fun spamD(hold: Int, duration: Int) {
         val spamTimer = TimeUtils.setInterval(fun () { dTap(hold) }, 0, hold * 2 + RandomUtils.randomIntInRange(0, hold/5))
         TimeUtils.setTimeout(fun () { spamTimer?.cancel() }, duration)
+    }
+
+    fun startRandomStrafe(min: Int, max: Int) {
+        randomStrafe = true
+        randomStrafeFunc(min, max)
+    }
+
+    fun stopRandomStrafe() {
+        randomStrafe = false
+    }
+
+    private fun randomStrafeFunc(min: Int, max: Int) {
+        if (randomStrafe) {
+            if (!(Movement.left() || Movement.right())) {
+                if (RandomUtils.randomBool()) {
+                    Movement.startLeft()
+                } else {
+                    Movement.startRight()
+                }
+            } else {
+                Movement.swapLeftRight()
+            }
+            TimeUtils.setTimeout(fun () { randomStrafeFunc(min, max) }, RandomUtils.randomIntInRange(min, max))
+        }
     }
 
 }
