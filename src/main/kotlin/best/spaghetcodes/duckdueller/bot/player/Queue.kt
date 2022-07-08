1e2d11cee9d93a64f05d1c11b1ff1e79687b1d8b
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-08 14:01:13 +0200

    Queue tracks inGame state

--- src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/Queue.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/Queue.kt
@@ -1,9 +1,19 @@
 package best.spaghetcodes.duckdueller.bot.player
 
 import best.spaghetcodes.duckdueller.DuckDueller
+import best.spaghetcodes.duckdueller.utils.ChatUtils
 import best.spaghetcodes.duckdueller.utils.TimeUtils
+import net.minecraft.network.play.server.S3EPacketTeams
+import net.minecraftforge.client.event.ClientChatReceivedEvent
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
 
 object Queue {
+
+    var inGame = false
 
     fun joinGame(msg: String) {
         DuckDueller.mc.thePlayer.sendChatMessage(msg)
@@ -14,6 +24,21 @@ object Queue {
         TimeUtils.setTimeout(fun () {
             DuckDueller.mc.thePlayer.sendChatMessage("/l")
         }, 500)
+    }
+
+    @SubscribeEvent
+    fun onChatMessage(ev: ClientChatReceivedEvent) {
+        val unformatted = ev.message.unformattedText
+        if (unformatted.matches(Regex(".* has joined \\(./2\\)!"))) {
+            inGame = true
+        }
+    }
+
+    @SubscribeEvent
+    fun onJoinWorld(ev: EntityJoinWorldEvent) {
+        if (DuckDueller.mc.thePlayer != null && ev.entity == DuckDueller.mc.thePlayer) {
+            inGame = false
+        }
     }
 
 }
