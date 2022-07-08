859b9334bd5df57eac7526871c852602d2987935
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-08 15:45:17 +0200

    listen for api key regeneration

--- src/main/kotlin/best/spaghetcodes/duckdueller/EventHandler.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/EventHandler.kt
@@ -1,5 +1,20 @@
 package best.spaghetcodes.duckdueller
 
+import best.spaghetcodes.duckdueller.utils.ChatUtils
+import best.spaghetcodes.duckdueller.utils.Config
+import net.minecraftforge.client.event.ClientChatReceivedEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
 class EventHandler() {
+
+    @SubscribeEvent
+    fun onChat(ev: ClientChatReceivedEvent) {
+        if (ev.message.unformattedText.contains("Your new API key is ")) {
+            val key = ev.message.unformattedText.split("Your new API key is ")[1]
+            Config.set("apiKey", key)
+            Config.save()
+            ChatUtils.info("Your API key has been saved!")
+        }
+    }
 
 }
