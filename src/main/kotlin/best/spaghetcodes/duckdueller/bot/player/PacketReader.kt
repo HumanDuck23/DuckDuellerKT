c8cd680803e7d0d7c5f3e316908e6e6ed000f566
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-07-20 14:01:13 +0200

    PacketReader use `when` instead of `if`s

--- src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/PacketReader.kt
+++ src/main/kotlin/best/spaghetcodes/duckdueller/bot/player/PacketReader.kt
@@ -25,47 +25,52 @@ class PacketReader : SimpleChannelInboundHandler<Packet<INetHandler>>(false) {
     }
 
     override fun channelRead0(ctx: ChannelHandlerContext, msg: Packet<INetHandler>) {
-        if (msg is S45PacketTitle && DuckDueller.mc.theWorld != null) {
-            TimeUtils.setTimeout(fun () {
-                if (msg.message != null) {
-                    val unformatted = msg.message.unformattedText.lowercase()
-                    if (unformatted.contains("won the duel!") && DuckDueller.mc.thePlayer != null) {
-                        var winner = ""
-                        var loser = ""
-                        var iWon = false
+        when (msg) {
+            is S45PacketTitle -> {
+                if (DuckDueller.mc.theWorld != null) {
+                    TimeUtils.setTimeout(fun () {
+                        if (msg.message != null) {
+                            val unformatted = msg.message.unformattedText.lowercase()
+                            if (unformatted.contains("won the duel!") && DuckDueller.mc.thePlayer != null) {
+                                var winner = ""
+                                var loser = ""
+                                var iWon = false
 
-                        val p = ChatUtils.removeFormatting(msg.message.unformattedText).split("won")[0].trim()
+                                val p = ChatUtils.removeFormatting(msg.message.unformattedText).split("won")[0].trim()
 
-                        if (unformatted.contains(DuckDueller.mc.thePlayer.displayNameString.lowercase())) {
-                            Session.wins++
-                            winner = DuckDueller.mc.thePlayer.displayNameString
-                            loser = DuckDueller.getBot()?.lastOpponentName ?: "---"
-                            iWon = true
-                        } else {
-                            Session.losses++
-                            ChatUtils.info("Adding $p to the list of players to dodge...")
-                            DuckDueller.getBot()?.playersLost?.add(p)
-                            winner = p
-                            loser = DuckDueller.mc.thePlayer.displayNameString
-                            iWon = false
+                                if (unformatted.contains(DuckDueller.mc.thePlayer.displayNameString.lowercase())) {
+                                    Session.wins++
+                                    winner = DuckDueller.mc.thePlayer.displayNameString
+                                    loser = DuckDueller.getBot()?.lastOpponentName ?: "---"
+                                    iWon = true
+                                } else {
+                                    Session.losses++
+                                    ChatUtils.info("Adding $p to the list of players to dodge...")
+                                    DuckDueller.getBot()?.playersLost?.add(p)
+                                    winner = p
+                                    loser = DuckDueller.mc.thePlayer.displayNameString
+                                    iWon = false
+                                }
+                                ChatUtils.info(Session.getSession())
+
+                                // Send the webhook embed
+                                val fields = WebHook.buildFields(arrayListOf(mapOf("name" to "Winner", "value" to winner, "inline" to "true"), mapOf("name" to "Loser", "value" to loser, "inline" to "true")))
+                                val footer = WebHook.buildFooter(ChatUtils.removeFormatting(Session.getSession()), "https://raw.githubusercontent.com/HumanDuck23/upload-stuff-here/main/duck_dueller.png")
+                                val author = WebHook.buildAuthor("Duck Dueller", "https://raw.githubusercontent.com/HumanDuck23/upload-stuff-here/main/duck_dueller.png")
+                                val thumbnail = WebHook.buildThumbnail("https://raw.githubusercontent.com/HumanDuck23/upload-stuff-here/main/duck_dueller.png")
+
+                                WebHook.sendEmbed(
+                                    Config.get("webhook").toString(),
+                                    WebHook.buildEmbed("${if (iWon) ":smirk:" else ":confused:"} Game ${if (iWon) "WON" else "LOST"}!", fields, footer, author, thumbnail, if (iWon) 0x66ed8a else 0xed6d66))
+
+                            }
                         }
-                        ChatUtils.info(Session.getSession())
-
-                        // Send the webhook embed
-                        val fields = WebHook.buildFields(arrayListOf(mapOf("name" to "Winner", "value" to winner, "inline" to "true"), mapOf("name" to "Loser", "value" to loser, "inline" to "true")))
-                        val footer = WebHook.buildFooter(ChatUtils.removeFormatting(Session.getSession()), "https://raw.githubusercontent.com/HumanDuck23/upload-stuff-here/main/duck_dueller.png")
-                        val author = WebHook.buildAuthor("Duck Dueller", "https://raw.githubusercontent.com/HumanDuck23/upload-stuff-here/main/duck_dueller.png")
-                        val thumbnail = WebHook.buildThumbnail("https://raw.githubusercontent.com/HumanDuck23/upload-stuff-here/main/duck_dueller.png")
-
-                        WebHook.sendEmbed(
-                            Config.get("webhook").toString(),
-                            WebHook.buildEmbed("${if (iWon) ":smirk:" else ":confused:"} Game ${if (iWon) "WON" else "LOST"}!", fields, footer, author, thumbnail, if (iWon) 0x66ed8a else 0xed6d66))
-
-                    }
+                    }, 1000)
                 }
-            }, 1000)
-        } else if (msg is S3EPacketTeams) {
-            Queue.teamPacket(msg)
+            }
+            is S3EPacketTeams -> {
+                Queue.teamPacket(msg)
+            }
         }
         ctx.fireChannelRead(msg)
     }
